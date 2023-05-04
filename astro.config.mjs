e8cf72a871d40e9fbebb75e9f9de51ec4d8c0082
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-05-04 22:36:35 +0530

    Update site name to pfmcoders.org

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://pfm-coders.github.io',
+  site: 'https://pfmcoders.org',
   integrations: [tailwind(), sitemap()]
 });
